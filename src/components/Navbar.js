62d57ca75277b65c42908a3dcc654f582502276f
singhv <vishesh.singh@biworldwide.com>
2019-06-30 17:32:58 -0500

    Accessing Consumer (second way)

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,27 @@
 import React, { Component } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
 class Navbar extends Component {
-  static contextType = ThemeContext;
   render() {
-    const { isLightTheme, light, dark } = this.context;
-    const theme = isLightTheme ? light : dark;
     return (
-      <nav
-        className="navbar"
-        style={{ background: theme.bg, color: theme.syntax }}
-      >
-        <h1>Context API</h1>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
+      <ThemeContext.Consumer>
+        {context => {
+          const { isLightTheme, light, dark } = context;
+          const theme = isLightTheme ? light : dark;
+          return (
+            <nav
+              className="navbar"
+              style={{ background: theme.bg, color: theme.syntax }}
+            >
+              <h1>Context API</h1>
+              <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+              </ul>
+            </nav>
+          );
+        }}
+      </ThemeContext.Consumer>
     );
   }
 }
