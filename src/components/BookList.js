b367a9d30819933b41a4b3d21fabd3f04e4f12bc
singhv <vishesh.singh@biworldwide.com>
2019-06-30 17:26:25 -0500

    Consuming Context in class-based components

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
-
+import { ThemeContext } from '../contexts/ThemeContext';
 export class BookList extends Component {
+  static contextType = ThemeContext;
   render() {
+    const { isLightTheme, light, dark } = this.context;
+    const theme = isLightTheme ? light : dark;
     return (
-      <div className="booklist">
+      <div
+        className="booklist"
+        style={{ background: theme.bg, color: theme.syntax }}
+      >
         <ul>
-          <li>The way of kings</li>
-          <li>The winds of glory</li>
-          <li>The final empire</li>
+          <li style={{ background: theme.ui }}>The way of kings</li>
+          <li style={{ background: theme.ui }}>The winds of glory</li>
+          <li style={{ background: theme.ui }}>The final empire</li>
         </ul>
       </div>
     );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
-
+import { ThemeContext } from '../contexts/ThemeContext';
 class Navbar extends Component {
+  static contextType = ThemeContext;
   render() {
+    const { isLightTheme, light, dark } = this.context;
+    const theme = isLightTheme ? light : dark;
     return (
-      <nav className="navbar">
+      <nav
+        className="navbar"
+        style={{ background: theme.bg, color: theme.syntax }}
+      >
         <h1>Context API</h1>
         <ul>
           <li>Home</li>
